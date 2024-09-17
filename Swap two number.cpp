02030e844a98f2133deabb96317bc4ee6bd89699
Anjali Kumari <166202845+An-jali-Kumari@users.noreply.github.com>
2024-09-17 23:25:13 +0530

    Add files via upload

---
+++ Swap two number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a , b , temp=0;
+cout<<"Program to swap two number by Anjali\n\n";
+cout<<"Enter First number (let a):";
+cin>>a;
+cout<<"\n Enter Second number (let b):";
+cin>>b;
+temp=a;
+a=b;
+b=temp;
+cout<<"\n After swap:-";
+cout<<"\n\t\t Value of a is:"<<a<<"\n\n\t\t Value of b is :"<<b;
+return 0;
+}
+
